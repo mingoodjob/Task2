850245979586c149368ca2725bf8b58b764a6ff2
이민기 <psjlmk@gmail.com>
2022-06-16 20:11:35 +0900

    fix user/views.py

--- user/views.py
+++ user/views.py
@@ -3,6 +3,8 @@ from requests import delete
 from rest_framework.response import Response
 from rest_framework import permissions
 from rest_framework.views import APIView
+from blog.models import Category, Article 
+from user.models import UserModel
 
 class UserApiView(APIView):
     """
@@ -32,9 +34,20 @@ class UserApiView(APIView):
 
 class UserPostView(APIView):
     """
-    
+
     """
     def get(self, request):
-        
-        return Response({'message': '겟!'})
+        posts = []
+        username = UserModel.objects.get(username=request.user)
+        articles = Article.objects.filter(author=username)
+        for article in articles:
+            titles = {
+                'author': article.author.username,
+                'title': article.title,
+                'content': article.content,
+                'category': article.category.name,
+                'date': article.date
+            }
+            posts.append(titles)
+        return Response({'post': posts})
     
